2b9985570421b18a05e30a4de5e0b05935cfafbf
thvascon <thvasconn@gmail.com>
2025-05-04 21:26:08 -0300

    Att1

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    let menu = document.querySelector(".menu-links")
+    let icon = document.querySelector(".mobile-icon")
+    menu.classList.toggle("open") 
+    icon.classList.toggle("open") 
+}
